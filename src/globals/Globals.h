f225b40f920df65ed6968bbce0aea8e2a2f215e6
amiralic <amiralic@students.cs.ubc.ca>
2024-10-09 14:52:20 -0700

    Created files for global variables

---
+++ src/globals/Globals.h
@@ -0,0 +1,13 @@
+//
+// File that is used for keep all global variables used for tuning gameplay (such as cameraRotationSpeed)
+//
+
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+namespace Globals {
+    extern float cameraRotationSpeed;
+    extern float dodgeMoveMag;
+}
+
+#endif //GLOBALS_H
