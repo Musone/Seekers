0009eb96fbd0c39a5b3a170b1bcf566ccccae449
Musone <yakobkh@gmail.com>
2024-10-11 00:19:54 -0700

    Narrowed down the bug. It doesnt render both models if I dont load both textures as the same time, before every calling texture bind.

--- src/app/Application.hpp
+++ src/app/Application.hpp
@@ -18,26 +18,6 @@ public:
     };
 
     void run_demo_texture() {
-        float player_vertices[] = {
-            // We have to manually map textures to our vertices. in UV coordinates,
-            // the bottom left of the texture is <0,0> and the top right is <1,1>.
-            -0.5f, -0.5f, 0.0f, 0.0f, // bottom left
-            0.0f, -0.5f, 1.0f, 0.0f, // bottom right
-            0.0f, 0.5f, 1.0f, 1.0f, // top right
-            -0.5f, 0.5f, 0.0f, 1.0f // top left
-        };
-        float skeleton_vertices[] = {
-            0.0f, -0.5f, 0.0f, 0.0f, // bottom left
-            0.5f, -0.5f, 1.0f, 0.0f, // bottom right
-            0.5f, 0.5f, 1.0f, 1.0f, // top right
-            0.0f, 0.5f, 0.0f, 1.0f // top left
-        };
-        // I set it up so that player and skeleton can use the same indices.
-        unsigned int indices[] = {
-            0, 1, 2,
-            0, 2, 3
-        };
-
         Renderer& renderer = Renderer::get_instance();
         // The renderer must be initialized before anything else.
         renderer.init(
@@ -47,52 +27,67 @@ public:
             true,
             false
         );
-        VertexArray player_vao;
-        VertexArray skeleton_vao;
-        player_vao.init();
-        skeleton_vao.init();
         
-        IndexBuffer ibo;
-        ibo.init(indices, Common::c_arr_count(indices));
-        
-        VertexBuffer player_vbo;
-        VertexBuffer skeleton_vbo;
-
-        player_vbo.init(player_vertices, sizeof(player_vertices));
-        skeleton_vbo.init(skeleton_vertices, sizeof(skeleton_vertices));
-
+        float player_vertices[] = {
+            // We have to manually map textures to our vertices. in UV coordinates,
+            // the bottom left of the texture is <0,0> and the top right is <1,1>.
+            -0.5f, -0.5f, 0.0f, 0.0f, // bottom left
+            0.0f, -0.5f, 1.0f, 0.0f, // bottom right
+            0.0f, 0.5f, 1.0f, 1.0f, // top right
+            -0.5f, 0.5f, 0.0f, 1.0f // top left
+        };
+        // I set it up so that player and skeleton can use the same indices.
+        unsigned int indices[] = {
+            0, 1, 2,
+            0, 2, 3
+        };
         VertexBufferLayout layout;
         // 2 floats form a vec2 that we use for position.
         layout.push<float>(2);
         // 2 float form a vec2 that we use for UV coordinates.
         layout.push<float>(2);
+        // The player and skeleton will use the same ibo because I made the indices match.
+        IndexBuffer ibo;
+        ibo.init(indices, Common::c_arr_count(indices));
         
+        Texture player_texture("player.png");
+        Texture skeleton_texture("skeleton.png");
+        
+        VertexArray player_vao;
+        VertexBuffer player_vbo;
+        player_vao.init();
+        player_vbo.init(player_vertices, sizeof(player_vertices));
         player_vao.add_buffer(player_vbo, layout);
+        const unsigned int player_texture_slot = 0;
+        player_texture.bind(player_texture_slot);
+        
+        float skeleton_vertices[] = {
+            0.0f, -0.5f, 0.0f, 0.0f, // bottom left
+            0.5f, -0.5f, 1.0f, 0.0f, // bottom right
+            0.5f, 0.5f, 1.0f, 1.0f, // top right
+            0.0f, 0.5f, 0.0f, 1.0f // top left
+        };
+        VertexArray skeleton_vao;
+        VertexBuffer skeleton_vbo;
+        skeleton_vao.init();
+        skeleton_vbo.init(skeleton_vertices, sizeof(skeleton_vertices));
         skeleton_vao.add_buffer(skeleton_vbo, layout);
+        const unsigned int skeleton_texture_slot = 1;
+        skeleton_texture.bind(skeleton_texture_slot);
 
         // src/shaders/TextureDemo.vs.glsl & src/shaders/TextureDemo.fs.glsl
         Shader shader;
         shader.init("TextureDemo");
-
-        Texture player_texture("player.png");
-        Texture skeleton_texture("skeleton.png");
-        player_texture.bind(0);
-        skeleton_texture.bind(1);
         
         while (!renderer.is_terminated()) {
             renderer.begin_draw();
 
-            ibo.bind();
-            shader.bind();
-
             // Render the player.
-            player_vao.bind();
-            shader.set_uniform_1i("u_texture", 0);
+            shader.set_uniform_1i("u_texture", player_texture_slot);
             renderer.draw(player_vao, ibo, shader);
 
             // Then render the skeleton.
-            skeleton_vao.bind();
-            shader.set_uniform_1i("u_texture", 1);
+            shader.set_uniform_1i("u_texture", skeleton_texture_slot);
             renderer.draw(skeleton_vao, ibo, shader);
 
             renderer.end_draw();
