104d5755947ceed3addb00837b63126f6777ddfb
Musone <yakobkh@gmail.com>
2024-10-10 21:27:35 -0700

    Refactored renderer. Created a test demo of its usage in application.hpp.

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,12 @@
 int main(void) {
     try {
         Application app;
+<<<<<<< HEAD
         // app.run_demo_basic();
         app.run_demo_texture();
+=======
+        app.run_test_loop();
+>>>>>>> c40bf06 (Refactored renderer. Created a test demo of its usage in application.hpp.)
     } catch (const std::exception& e) {
         const std::string message = std::string(e.what());
         Log::log_error_and_terminate(message, __FILE__, __LINE__);
