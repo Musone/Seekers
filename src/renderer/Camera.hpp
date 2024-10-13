0697d759652c832157fde417ff87aef4dcc60b72
UBC Student <samiam7@students.cs.ubc.ca>
2024-10-13 00:19:12 -0700

    Update Camera.hpp to use new Transform utils class.

--- src/renderer/Camera.hpp
+++ src/renderer/Camera.hpp
@@ -1,8 +1,7 @@
 #pragma once
 
+#include <utils/Transform.hpp>
 #include <glm/glm.hpp>
-
-#define PI 3.1415926535
 
 class Camera {
     glm::vec3 m_position;
@@ -21,14 +20,14 @@ class Camera {
     float m_far;
     float m_near;
 public:
-    Camera() : 
+    Camera(const int& window_width, const int& window_height) : 
         m_position(0), 
         m_rotation(0), 
         m_translate(glm::mat4(1.0f)), 
         m_rotate(glm::mat4(1.0f)),
         m_fov(PI / 2),
-        m_width(1920),
-        m_height(1280),
+        m_width(window_width),
+        m_height(window_height),
         m_far(100),
         m_near(-1) {
         _update_view_matrix();
@@ -38,33 +37,15 @@ public:
     ~Camera() {}
 
     glm::vec3 rotate_to_camera_direction(const glm::vec3& input) {
-        const glm::mat4 rotate_z = {
-            glm::cos(m_rotation.z),  glm::sin(m_rotation.z), 0, 0,
-           -glm::sin(m_rotation.z),  glm::cos(m_rotation.z), 0, 0,
-            0,                          0,                   1, 0,
-            0,                          0,                   0, 1
-        };
-        const glm::mat4 rotate_y = {
-            glm::cos(m_rotation.y),  0, -glm::sin(m_rotation.y), 0,
-            0,                       1,  0,                      0,
-            glm::sin(m_rotation.y),  0,  glm::cos(m_rotation.y), 0,
-            0,                       0,  0,                      1
-        };
-        const glm::mat4 rotate_x = {
-            1,  0,                        0,                       0,
-            0,  glm::cos(m_rotation.x),   glm::sin(m_rotation.x),  0,
-            0, -glm::sin(m_rotation.x),   glm::cos(m_rotation.x),  0,
-            0,  0,                        0,                       1
-        };
-        auto result = rotate_z * rotate_y * rotate_x * glm::vec4({ input.x, input.y, input.z, 1 });
+        const auto result = Transform::create_rotation_matrix(m_rotation) * glm::vec4({ input.x, input.y, input.z, 1 });
         return { result.x, result.y, result.z };
     }
 
     glm::mat4 get_view_project_matrix() { return m_view_project; }
 
-    glm::vec3 get_position() { return m_position; }
+    glm::vec3 get_position() const { return m_position; }
 
-    glm::vec3 get_rotation() { return m_rotation; }
+    glm::vec3 get_rotation() const { return m_rotation; }
 
     glm::vec2 project_to_camera(const glm::vec3& world_position) const {
         glm::vec4 ndc = m_view_project * glm::vec4(world_position, 1);
@@ -74,37 +55,13 @@ public:
 
     void set_position(const glm::vec3& new_position) {
         m_position = new_position;
-        m_translate = {
-            1,                0,               0,              0,
-            0,                1,               0,              0,
-            0,                0,               1,              0,
-            -new_position.x, -new_position.y, -new_position.z, 1
-        };
+        m_translate = Transform::create_translation_matrix(-new_position);
         _update_view_matrix();
     }
 
     void set_rotation(const glm::vec3& new_rotation) {
         m_rotation = new_rotation;
-        const glm::mat4 rotate_z = {
-            glm::cos(-new_rotation.z),  glm::sin(-new_rotation.z), 0, 0,
-           -glm::sin(-new_rotation.z),  glm::cos(-new_rotation.z), 0, 0,
-            0,                          0,                         1, 0,
-            0,                          0,                         0, 1
-        };
-        const glm::mat4 rotate_y = {
-            glm::cos(-new_rotation.y),  0, -glm::sin(-new_rotation.y), 0,
-            0,                          1,  0,                         0,
-            glm::sin(-new_rotation.y),  0,  glm::cos(-new_rotation.y), 0,
-            0,                          0,  0,                         1
-        };
-
-        const glm::mat4 rotate_x = {
-            1,  0,                           0,                          0,
-            0,  glm::cos(-new_rotation.x),   glm::sin(-new_rotation.x),  0,
-            0, -glm::sin(-new_rotation.x),   glm::cos(-new_rotation.x),  0,
-            0,  0,                           0,                          1
-        };
-        m_rotate = rotate_x * rotate_y * rotate_z;
+        m_rotate = Transform::create_inverse_rotation_matrix(new_rotation);
         _update_view_matrix();
     }
 
@@ -119,6 +76,7 @@ private:
         };
         m_view_project = m_project * m_view;
     }
+
     void _update_view_matrix() {
         m_view = m_rotate * m_translate;
         m_view_project = m_project * m_view;
