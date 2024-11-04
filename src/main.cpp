b478e38c42fbe9e11063d716ab0f2c5370a98cb4
Musone <yakobkh@gmail.com>
2024-11-03 21:05:17 -0800

    Made the game use NVIDIA graphics card, thank god

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,14 @@
 #include <app/Application.hpp>
 #include <Testing.hpp>
 
+#if __APPLE__
+#else
+extern "C" 
+{
+  __declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
+};
+#endif // !__APPLE__
+
 int main(void) {
     try {
         Application app;
