06577a8adea976f2bdbf850e5c956b8707e81034
UBC Student <samiam7@students.cs.ubc.ca>
2024-11-18 12:50:37 -0800

    fix tree implementation to include rotation

--- src/systems/OpenWorldMapCreatorSystem.hpp
+++ src/systems/OpenWorldMapCreatorSystem.hpp
@@ -16,7 +16,7 @@ namespace OpenWorldMapCreatorSystem {
         return true;
     }
 
-    inline void create_forest(Registry& registry, const glm::vec2& center_position, int num_trees = 500, float min_distance = 8.0f) {
+    inline void create_forest(Registry& registry, const glm::vec2& center_position, int num_trees = 100, float min_distance = 30.0f) {
         std::random_device rd;
         std::mt19937 gen(rd());
         
@@ -24,6 +24,7 @@ namespace OpenWorldMapCreatorSystem {
 
         std::uniform_real_distribution<float> dist_radius(0.0f, forest_radius);
         std::uniform_real_distribution<float> dist_angle(0.0f, 2.0f * PI);
+        std::uniform_real_distribution<float> dist_rotation(0.0f, 2.0f * PI);
 
         std::vector<glm::vec2> tree_positions;
         int attempts = 1000; // prevent infinite loops
@@ -32,13 +33,14 @@ namespace OpenWorldMapCreatorSystem {
         while (trees_created < num_trees && attempts > 0) {
             float radius = dist_radius(gen);
             float angle = dist_angle(gen);
+            float tree_rotation = dist_rotation(gen);
             
             glm::vec2 offset(radius * cos(angle), radius * sin(angle));
 
             glm::vec2 new_pos = center_position + offset;
 
             if (is_position_valid(new_pos, tree_positions, min_distance)) {
-                EntityFactory::create_tree(registry, new_pos);
+                EntityFactory::create_tree(registry, new_pos, tree_rotation);
                 tree_positions.push_back(new_pos);
                 trees_created++;
             }
@@ -50,10 +52,10 @@ namespace OpenWorldMapCreatorSystem {
     inline void populate_open_world_map(Registry& registry) {
         // TODO: add everything here Sam
         //EntityFactory::create_tree(registry, glm::vec2(0, 0)); //example
-        create_forest(registry, glm::vec2(160.f, 160.f));
-        create_forest(registry, glm::vec2(-20.f, 160.f));
-        create_forest(registry, glm::vec2(-180.f, 160.f), 150);
-        EntityFactory::create_rock(registry, {50, 50});                         // example of rock
-        EntityFactory::create_tree(registry, glm::vec2(40.f, 40.f), PI);     // example of tree with angle
+        create_forest(registry, glm::vec2(0.f, 0.f));
+        // create_forest(registry, glm::vec2(-20.f, 160.f));
+        // create_forest(registry, glm::vec2(-180.f, 160.f), 150);
+        // EntityFactory::create_rock(registry, {50, 50});                         // example of rock
+        // EntityFactory::create_tree(registry, glm::vec2(40.f, 40.f), PI);     // example of tree with angle
     }
 };
